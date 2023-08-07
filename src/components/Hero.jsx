40c0bd4a270c09acbedbbbd7ed45db986586bb50
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-08 03:56:08 +0530

    update indexHtml

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,8 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Niranjan</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className="sm-block hidden" />interfaces and web applications</p>
+          {/* <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className="sm-block hidden" />interfaces and web applications</p> */}
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop web applications</p>
         </div>
       </div>
       <br /><br />
