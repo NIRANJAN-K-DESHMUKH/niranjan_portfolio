63a32cdb23e33488e44001865d5b47a4e2fa4341
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-30 16:42:06 +0530

    update

---
+++ src/components/TechWrapper.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Tech} from "./components";
+import { SectionWrapper } from "../hoc";
+
+const TechWrapper = () => {
+  return (
+    <div className="h-100px w-200px">
+      {/* <Tech /> */}
+      hello
+    </div>
+  );
+};
+
+export default SectionWrapper(TechWrapper, "");
