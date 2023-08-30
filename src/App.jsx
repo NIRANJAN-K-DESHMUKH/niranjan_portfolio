8833c53f270652c1e51c393408de10c00cff957f
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-30 16:11:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
           <Hero />
         </div>
         <About />
-        {/* <Tech /> */}
+        <Tech />
         <Feedbacks />
         <Experience />
         <Works />
