81fec4780a2b2f3f3f292eb23ea9546c0166b9c1
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-30 08:57:09 +0530

    try earth at computers place

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {styles} from "../styles";
 import {motion} from "framer-motion";
-import {ComputersCanvas} from "./canvas";
+// import {ComputersCanvas} from "./canvas";
+import { EarthCanvas } from "./canvas";
+
 
 
 const Hero = () => {
@@ -19,7 +21,8 @@ const Hero = () => {
         </div>
       </div>
       <br /><br />
-      <ComputersCanvas />
+      <EarthCanvas />
+      {/* <ComputersCanvas /> */}
       
       <div 
       className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'
