4c89a9792052208df45acee2f6817dfa0381ce1c
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-30 09:49:38 +0530

    update compCanvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -72,9 +72,8 @@ const ComputersCanvas = () => {
                     minPolarAngle={Math.PI / 2}
                 />
                 <Computers isMobile={isMobile} />
-                <Preload all />
             </Suspense>
-            
+            <Preload all />
       </Canvas>
   )
 }
