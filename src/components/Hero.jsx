222696ab84b8b52ef158936500e68d572bc24df0
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-30 14:46:46 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,19 +25,23 @@ const Hero = () => {
       className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'
       title="You can rotate the 3D Computer :)"
       >
-          <div className='w-[64px] h-[35px] rounded-3xl border-4 border-secondary flex justify-left items-start p-2'>
+        <a href='#about'>
+          <div className='w-[80px] h-[46px] rounded-3xl border-4 border-secondary flex justify-left items-start p-2'>
             <motion.div
               animate={{
-                x: [0, 0, 24],
+                y: [24, 0, 0],
               }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
+              className='w-40 h-6 rounded-full bg-secondary flex justify-center'
+            >
+              Scroll
+            </motion.div>
           </div>
+          </a>
       </div>
 
     </section>
