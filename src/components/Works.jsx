108133662ae81df0a32cef4c32d0856273142057
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-31 11:55:57 +0530

    update projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -20,7 +20,10 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={slideIn("left", "tween", 0.2, 1)}
+    <motion.div 
+
+
+
     title="Click on github icon to view the repository"
     >
       <Tilt
@@ -82,7 +85,7 @@ const Works = () => {
 
       <div className='w-full flex'>
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+              variants={slideIn("left", "tween", 0.2, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
           Following projects showcases my skills and experience through
