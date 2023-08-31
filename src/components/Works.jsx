7e72e05e6f740aaa71c0b0066d23c6c404a05e80
Niranjan K D <niranjandeshmukh371@gmail.com>
2023-08-31 12:56:55 +0530

    update projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -76,7 +76,9 @@ const ProjectCard = ({
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
+      <motion.div 
+      // variants={textVariant()}
+      >
         <p className={`${styles.sectionSubText}`}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
